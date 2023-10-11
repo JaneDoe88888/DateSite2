ae65ce092b4fe92850d64b7bdb0cd6b71ac539f3
SyuzannaVirabyan <s89169906201@gmail.com>
2023-10-11 22:28:05 +0300

    фото

---
+++ DateSite/files/static/script.js
@@ -0,0 +1,9 @@
+function toggleModal(url){
+let modal = document.querySelector('.gallery-modal')
+    // let img = document.createElement('img')
+    // const src = '../uploads/' + url.split('/').at(-1)
+    // img.src = src
+    // modal.querySelector('.modal-content').append(img)
+    // modal.classList.toggle('is-active')
+    window.open(url)
+}
