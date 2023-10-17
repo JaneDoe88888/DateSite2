316d54d7c478065e8505efcec81c3eb1590e0a1a
SyuzannaVirabyan <s89169906201@gmail.com>
2023-10-17 22:43:44 +0300

    setting

--- DateSite/head/forms.py
+++ DateSite/head/forms.py
@@ -1,7 +1,10 @@
+
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 from users.bulma_mixin import BulmaMixin
+from .models import *
+
 
 from .models import Image
 
@@ -15,6 +18,25 @@ class SignInForm(BulmaMixin, AuthenticationForm):
         model = User
         fields = ['username', 'password']
 
+class EditProfileForm(BulmaMixin, forms.ModelForm):
+    username = forms.CharField(label='Введите имя')
+    birthday = forms.IntegerField(label='Ваш возраст', validators=[MinValueValidator(limit_value=18)])
+    city = forms.CharField(label='Ваш город')
+    job = forms.CharField(label='Ваша работа')
+    hobbies = forms.CharField(label='Ваше хобби')
+    status = forms.ChoiceField(label='Ваше семейное положение', choices=STATUS_CHOICE)
+
+    class Meta:
+        model = Profile
+        fields = [
+            'username',
+            'birthday',
+            'city',
+            'job',
+            'hobbies',
+            'status'
+        ]
+
 
 class UploadForm(forms.ModelForm):
     image = forms.ImageField()
@@ -22,3 +44,19 @@ class UploadForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ['image', ]
+
+
+class SearchForm(BulmaMixin, forms.ModelForm):
+    gender = forms.ChoiceField(label='Выберите пол', choices=GENDER)
+    age_min = forms.IntegerField(label='Минимальный возраст')
+    age_max = forms.IntegerField(label='Максимальный возраст')
+    city = forms.CharField(label='Введите город', max_length=255)
+
+    class Meta:
+        model = Profile
+        fields = [
+            'gender',
+            'age_min',
+            'age_max',
+            'city',
+        ]
--- DateSite/head/models.py
+++ DateSite/head/models.py
@@ -16,20 +16,38 @@ GENDER_CHOICE = [
 
 
 # class User(models.Model):
-#     username = models.CharField(max_length=255)
-#     birthday = models.DateTimeField()
-#     email = models.EmailField()
-#     password = models.CharField(max_length=255)
-#     gender = models.CharField(max_length=255, choices=GENDER_CHOICE)
 
+
+  #   email = models.EmailField()
+   #  password = models.CharField(max_length=255)
+
+
+STATUS_CHOICE = [
+    ('В активном поиске', 'В активном поиске'),
+    ('Влюблен(а)', 'Влюблен(а)'),
+    ('Встречается', 'Встречается'),
+    ('В гражданском браке', 'В гражданском браке'),
+    ('Помолвлен(а)', 'Помолвлен(а)'),
+    ('Женат(Замужем)', 'Женат(Замужем)'),
+    ('Все сложно', 'Все сложно'),
+    ('Не женат(Не замужем)', 'Не женат(Не замужем)')
+]
+GENDER = [
+    ('male', 'мужчина'),
+    ('female', 'женщина'),
+    ('trans', 'трансформер')
+]
 
 class Profile(models.Model):
+    username = models.CharField(max_length=255)
+    birthday = models.DateTimeField()
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     about = models.TextField()
     job = models.CharField(max_length=255)
     hobbies = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255, choices=GENDER_CHOICE)
     image = models.ImageField()
     images = models.ForeignKey('head.Image', on_delete=models.CASCADE)
 
